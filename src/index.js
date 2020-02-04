16ac638e91f523662fbb45dd31c48bf3b785bfc6
ignacio <ignaciomenchaca666@gmail.com>
2020-02-04 01:56:14 -0600

    deploy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import app from './app'
 import '@babel/polyfill';
+import db from "./app/models";
+
 
 async function main() {
 
-    await app.listen(process.env.PORT || 3000, () => {
-        console.log('listen on port 3000')
-    });
+    db.sequelize.sync().then( async function() {
+        await app.listen(process.env.PORT || 3000, () => {
+            console.log('listen on port 3000')
+        });
+    })
 }
 
 main();
