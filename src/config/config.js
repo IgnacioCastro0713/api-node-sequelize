1f1763f9d806c797f9e193adf558662dae149582
ignacio <ignaciomenchaca666@gmail.com>
2020-02-04 00:04:19 -0600

    deploy

--- src/config/config.js
+++ src/config/config.js
@@ -16,7 +16,7 @@ export default {
 	  "operatorsAliases": false
 	},
 	"production": {
-	  "use_env_variable": "DATABASE_URL",
+	  "use_env_variable": process.env.DATABASE_URL,
 	  "dialect": "postgres",
 	  "dialectOptions": {
 		"ssl": true
