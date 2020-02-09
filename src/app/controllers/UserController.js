1563e4605ff757c1d9f6be8d12676e197ab477f5
ignacio <ignaciomenchaca666@gmail.com>
2020-02-08 22:29:30 -0600

    UserController.js complete TODO: hash password

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,111 @@
-import { User } from '../models/';
+import { User, Project } from '../models/';
 
-export const getAllUsers = async (req, res) => {};
+export const getAllUsers = async (req, res) => {
+  try {
 
-export const createUser = async (req, res) => {};
+	let users = await User.findAll();
 
-export const getOneUser = async (req, res) => {};
+	return res.json({users});
+  } catch (e) {
+	res.status(500).json({
+	  message: 'Something goes wrong',
+	  users: {}
+	})
+  }
+};
 
-export const updateUser = async (req, res) => {};
+export const createUser = async (req, res) => {
 
-export const destroyUser = async (req, res) => {};
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.create({
+	  name, email, password
+	});
+
+    return res.json({user});
+
+  } catch (e) {
+	res.status(500).json({
+	  message: 'Something goes wrong',
+	  user: {}
+	})
+  }
+
+};
+
+export const getOneUser = async (req, res) => {
+
+  const { id } =  req.params;
+
+  try {
+	let user = await User.findOne({
+	  where: { id },
+	  include: [
+		{model: Project, attributes: ['name']}
+	  ]
+	});
+
+	return res.json({user});
+
+  } catch (e) {
+	res.status(500).json({
+	  message: 'Something goes wrong',
+	  user: {}
+	})
+  }
+};
+
+export const updateUser = async (req, res) => {
+
+  const { id } =  req.params;
+  const { name, email, password } = req.body;
+
+  let user = await User.findOne({
+	where:{id}
+  });
+
+  if (!user) {
+	return res.json({
+	  message: 'This user does not exist',
+	  user: {}
+	});
+  }
+
+  try {
+	await User.update({
+	  name, email, password
+	});
+
+	return res.json({ message: 'User Updated Successfully', user } );
+  } catch (e) {
+	res.status(500).json({
+	  message: 'Something goes wrong',
+	  user: {}
+	})
+  }
+
+};
+
+export const destroyUser = async (req, res) => {
+
+  const { id } =  req.params;
+
+  try {
+
+	let rowCount =  await User.destroy({
+	  where: {id}
+	});
+
+	return res.json({
+	  message: 'User Deleted Successfully',
+	  count: rowCount
+	});
+
+  } catch (e) {
+	res.status(500).json({
+	  message: 'Something goes wrong',
+	  project: {}
+	})
+  }
+};
