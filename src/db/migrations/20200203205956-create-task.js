c9e6fdce20dd06bc445f6095b108d2cfd750d2d7
ignacio <ignaciomenchaca666@gmail.com>
2020-02-09 23:30:21 -0600

    rename tables in migrations

--- src/db/migrations/20200203205956-create-task.js
+++ src/db/migrations/20200203205956-create-task.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('tasks', {
+    return queryInterface.createTable('Tasks', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -35,6 +35,6 @@ module.exports = {
 
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('tasks');
+    return queryInterface.dropTable('Tasks');
   }
 };
