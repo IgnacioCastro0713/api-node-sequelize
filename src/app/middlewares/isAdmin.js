37f580653a7b1956478875ab73ee1a4d5b54d3f3
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-06-28 19:26:23 -0500

    middleware admin completed swagger user section header admin implemented

--- src/app/middlewares/isAdmin.js
+++ src/app/middlewares/isAdmin.js
@@ -1,13 +1,17 @@
 import Role from '../../helpers/role';
 
 export default (req, res, next) => {
-  const isAdmin = req.headers.admin === "true";
+
+  let requestHandler = req.headers.admin;
+
+  if (typeof requestHandler == 'boolean') {
+    requestHandler = requestHandler.toString();
+  }
+
+  const isAdmin = requestHandler === "true";
   if (isAdmin === Role.ADMIN) {
     next();
   } else {
-    return res.status(403).json({
-      status: 403,
-      message: 'Sorry but you are not an admin and you do not have access'
-    });
+    return res.status(403).json({ status: 403, message: 'Sorry but you are not an admin and you do not have access' });
   }
 }
